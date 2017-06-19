23a1734118f38367aac46d0069950dbf308d192a
yamelnik <amosmelnik@gmail.com>
2017-06-19 20:00:17 +0300

    Added initial project.

---
+++ Common/Common/IDbHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public interface IDbHandler
+    {
+        User GetUser(Guid userId);
+
+        IEnumerable<Tag> GetTagsByUser(Guid userId);
+
+        IEnumerable<Tag> GetAllTags();
+
+        IEnumerable<User> GetUsersByTags(IEnumerable<Tag> tags);
+
+        IEnumerable<User> GetUsersByString(string str);
+
+        IEnumerable<User> GetAllUsers();
+
+        Tag GetTag(string tagText);
+
+        IEnumerable<Tag> GetAllTagsInCategory(string categoryName);
+
+        void AddNewTag(Tag newTag);
+
+        void AddNewUser(User newUser);
+
+        void AddTagToUser(Guid userId, Tag newTag);
+
+        void UpdateUser(Guid userId, User newUser);
+    }
+}
