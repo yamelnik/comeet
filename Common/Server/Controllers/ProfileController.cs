3abdc37653f6059c4951f6ca16484b5b883b1310
Miryam <miryampeleg@gmail.com>
2017-06-19 20:56:55 +0300

    Added controllers.

---
+++ Common/Server/Controllers/ProfileController.cs
@@ -0,0 +1,42 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.UI.WebControls;
+
+namespace Api.Controllers
+{
+    public class ProfileController : ApiController
+    {
+        private IDbHandler DbHandler;
+
+        public ProfileController(IDbHandler dbHandler)
+        {
+            DbHandler = dbHandler;
+        }
+
+        public User Get([FromBody]Guid userId)
+        {
+            return DbHandler.GetUser(userId);
+        }
+
+        public void Post([FromBody] User user)
+        {
+            DbHandler.AddNewUser(user);
+        }
+
+        [HttpPut]
+        public void AddTag([FromBody]Guid userGuid, [FromBody]Tag tag)
+        {
+            DbHandler.AddTagToUser(userGuid, tag);
+        }
+        
+        public void Put([FromBody]Guid userId, [FromBody]User user)
+        {
+            DbHandler.UpdateUser(userId, user);
+        }
+    }
+}
---
+++ Common/Server/Controllers/TagsController.cs
@@ -0,0 +1,30 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    public class TagsController : ApiController
+    {
+        private IDbHandler DbHandler;
+        TagsController(IDbHandler dbHandler)
+        {
+            DbHandler = dbHandler;
+        }
+        
+        public IEnumerable<Tag> Get()
+        {
+            return DbHandler.GetAllTags();
+        }
+        
+        public IEnumerable<Tag> Get(string category)
+        {
+            return DbHandler.GetAllTagsInCategory(category);
+        }
+        
+    }
+}
